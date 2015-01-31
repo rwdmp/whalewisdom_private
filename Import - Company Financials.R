f957bb7bd48878bdc70c6ddb49d9ff398c576f3f
espin086 <espin086@gmail.com>
2015-01-31 00:54:01 -0800

    Updated company financials program, pending productionalization

--- Import - Company Financials.R
+++ Import - Company Financials.R
@@ -3,14 +3,28 @@
 
 library(quantmod)
 
-#Specifying symbol to get data for.
-ticker <- c("DIS")
+#Set working directory.
+
+infiles <- setwd("C:/Users/ESPIJ090.WDW/whalewisdom - data")
+
+stocks <- read.csv("stocks_clean.csv")
+
+#Specifying symbols to get data for.
+tickers <- as.character(stocks[,3])
+ticker <-c("DIS")
 
 #Retrieving data.
-getFinancials(Symbol=ticker, src="google")
+
+getFinancials(Symbol="DIS", src="google")
 DIS <- viewFinancials(DIS.f, type=c('BS','IS','CF'), period=c('Q'))
 
 #Transforming data into a more useful form.
-DIS_t <- t(DIS)
+DIS_t <- as.data.frame(t(DIS))
 DIS_t2 <- cbind(DIS_t,ticker)
+
+write.csv(DIS_t2,"Example_Company_Financials.csv")
+
+
+
+
 
